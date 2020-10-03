2411abd932f0144090eb9bc7212a1326158883a1
Brian Cappello <briancappello@gmail.com>
2020-10-03 09:49:43 -0400

    create time bucket keys for all symbols on alpaca in the init command

--- cmd/create/main.go
+++ cmd/create/main.go
@@ -3,9 +3,18 @@
 package create
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/alpacahq/marketstore/v4/contrib/alpaca/api"
+	"github.com/alpacahq/marketstore/v4/executor"
+	"github.com/alpacahq/marketstore/v4/planner"
+	"github.com/alpacahq/marketstore/v4/utils"
+	"github.com/alpacahq/marketstore/v4/utils/io"
+	"github.com/alpacahq/marketstore/v4/utils/log"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +23,8 @@ const (
 	short   = "Creates a new mkts.yml file"
 	long    = "This command creates a new mkts.yml file in the current directory"
 	example = "marketstore init"
+
+	configFilePath = "mkts.yml"
 )
 
 var (
@@ -35,11 +46,52 @@ func executeInit(*cobra.Command, []string) error {
 	if err != nil {
 		return err
 	}
+
 	// write to current directory.
-	err = ioutil.WriteFile("mkts.yml", data, 0644)
+	_, err = os.Stat(configFilePath)
+	if os.IsNotExist(err) {
+		err = ioutil.WriteFile(configFilePath, data, 0644)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := utils.InstanceConfig.Load(configFilePath); err != nil {
+		return err
+	}
+	executor.NewInstanceSetup(utils.InstanceConfig.RootDirectory, true, true, true, true)
+
+	api.SetCredentials(
+		utils.InstanceConfig.Alpaca.APIKey,
+		utils.InstanceConfig.Alpaca.APISecret,
+	)
+
+	assets, err := api.ListAssets()
 	if err != nil {
 		return err
 	}
-	// create a new directory to store data.
-	return os.Mkdir("data", 0700)
+
+	ohlcvDataShapes := []io.DataShape{
+		{Name: "Epoch", Type: io.EPOCH},
+		{Name: "Open", Type: io.FLOAT32},
+		{Name: "High", Type: io.FLOAT32},
+		{Name: "Low", Type: io.FLOAT32},
+		{Name: "Close", Type: io.FLOAT32},
+		{Name: "Volume", Type: io.INT32},
+	}
+
+	for _, asset := range assets {
+		if strings.Contains(asset.Symbol, "-") || len(asset.Symbol) > 4 {
+			continue
+		}
+
+		log.Info("Creating timebuckets for %v", asset.Symbol)
+		if err := create(io.NewTimeBucketKey(asset.Symbol + "/1Min/OHLCV"), ohlcvDataShapes); err != nil {
+			log.Warn("%v", err)
+		}
+		if err := create(io.NewTimeBucketKey(asset.Symbol + "/1D/OHLCV"), ohlcvDataShapes); err != nil {
+			log.Warn("%v", err)
+		}
+	}
+	return nil
 }
